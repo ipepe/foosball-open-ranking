27cb36cc7453b980bd5a430cfb50370e43cbf48c
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2015-10-01 23:07:54 +0200

    rake 4 azure

--- deploy.cmd
+++ deploy.cmd
@@ -97,6 +97,8 @@ IF EXIST "%DEPLOYMENT_TARGET%\Gemfile.lock" (
   echo Executing bundle install
 
   PUSHD "%DEPLOYMENT_TARGET%"
+  %JRUBY_EXE% -S "%JRUBY_GEM_CMD%" uninstall rake
+  %JRUBY_EXE% -S "%JRUBY_GEM_CMD%" uninstall rake
 
   %JRUBY_EXE% -S "%JRUBY_BUNDLER_CMD%" install --without development:test --path vendor/bundle --binstubs vendor/bundle/bin -j4 --deployment
   IF !ERRORLEVEL! NEQ 0 goto error
@@ -107,7 +109,6 @@ IF EXIST "%DEPLOYMENT_TARGET%\Gemfile.lock" (
 PUSHD "%DEPLOYMENT_TARGET%"
 SET RAILS_ENV=production
 echo Precompiling assets in %cd%
-%JRUBY_EXE% -S "%JRUBY_BUNDLER_CMD%" update rake
 %JRUBY_EXE% -S "%JRUBY_BUNDLER_CMD%" exec rake assets:precompile
 POPD
 
