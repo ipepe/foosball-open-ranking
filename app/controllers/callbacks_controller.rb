d31de4722a88541916f5d71428b1f81a5b6dce2e
Patryk Ptasinski <patryk.ptasinski@in4mates.com>
2016-04-01 14:32:13 +0200

    Added remember me to omniauth

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    remember_me(@user)
     sign_in_and_redirect @user
   end
 end
